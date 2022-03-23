0f110837d1123c2b136c116332acaa15f175e49d
Natanna <natannacortez@gmail.com>
2022-03-23 19:43:07 -0300

    prepare to merge

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -3,10 +3,12 @@ import { submitPost, getTimelinePosts } from "../controllers/postController.js";
 import postSchema from "../schemas/postSchema.js";
 import validateSchema from "../middlewares/validateSchemaMW.js";
 import { validateToken } from "../middlewares/validateTokenMW.js";
+import { deletePost } from "../controllers/postController.js";
 
 const postRouter = Router();
 
 postRouter.post("/post", validateToken, validateSchema(postSchema), submitPost);
 postRouter.get("/timeline", validateToken, getTimelinePosts);
+postRouter.delete('/post/:id',validateToken, deletePost);
 
 export default postRouter;
