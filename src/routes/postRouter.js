0c94df83377a16d63313f4cbf777eaaba9cb7de0
Natanna <natannacortez@gmail.com>
2022-03-23 12:27:21 -0300

    fix: route post

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { deletePost } from "../controllers/postController.js";
 
 const postRouter=Router();
-postRouter.delete('/post/:id',deletePost);
+postRouter.delete('/post/:id', deletePost);
 
 export default postRouter;
