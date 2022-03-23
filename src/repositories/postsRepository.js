ca0e428914ca47df8fafee43df51656270c7cee2
pedro <pedrohperoni@gmail.com>
2022-03-23 16:41:58 -0300

    feat: GET/timeline posts

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -1,13 +1,30 @@
 import connection from "../db.js";
 
 async function create(post) {
-    connection.query(`
+  connection.query(
+    `
         INSERT INTO posts
         (link, "textPost", "userId")
         VALUES ($1, $2, $3)
-    `, [post.link, post.text, post.userId]);
+    `,
+    [post.link, post.text, post.userId]
+  );
+}
+
+async function getTimeline() {
+  return connection.query(`
+   SELECT 
+      posts.*,
+      users.name AS "userName",
+      users.image AS "userImage"
+   FROM posts
+   JOIN users ON users.id = posts."userId"
+    ORDER BY date DESC LIMIT 20`);
 }
 
 export const postsRepository = {
-    create
-}
+  create,
+  getTimeline,
+};
+
+// SELECT * FROM posts ORDER BY date DESC LIMIT 20`
