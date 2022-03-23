6129e216ff573b8459db55f4c97c1c4570481c59
ariel <ariel1242009@gmail.com>
2022-03-23 18:45:37 -0300

    prepare to merge

--- src/schemas/postSchema.js
+++ src/schemas/postSchema.js
@@ -2,7 +2,10 @@ import joi from "joi";
 
 const postSchema = joi.object({
     link: joi.string().uri().required(),
-    text: joi.string()
+    text: joi.string(),
+    title: joi.string().required(),
+    description: joi.string(),
+    image: joi.string().uri()
 });
 
 export default postSchema;
