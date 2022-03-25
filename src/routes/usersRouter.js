e4319d20b7bbd50d1ae5e2e961ec29d1cd9df305
Moises Junior <m.junior@eq.ufrj.br>
2022-03-24 21:55:45 -0300

    feat: sending user posts to front

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { createUser } from '../controllers/usersController.js';
+import { createUser, getUser } from '../controllers/usersController.js';
 import validateSchema from '../middlewares/validateSchemaMW.js';
+import { validateToken } from '../middlewares/validateTokenMW.js';
 import userSchema from '../schemas/userSchema.js';
 
 const usersRouter = Router();
 
 usersRouter.post('/users', validateSchema(userSchema), createUser);
+usersRouter.get('/user/:id', validateToken, getUser);
 
 export default usersRouter;
